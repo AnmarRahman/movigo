940557dff22ebfd5dba3d07d5e123cf0f41a80ea
AnmaritoS <95002766+AnmaritoS@users.noreply.github.com>
2023-06-15 22:56:22 -0400

    hero slideshow in progress

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,33 +1,75 @@
-import Image from "next/image";
-import heroImg from "../resources//clinteastwood.png";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
-// resources\5484546-good-bad-and-ugly-wallpapers.jpg
-import { useRouter } from "next/router";
-import MoviesList from "./MoviesList";
-
 function Hero({ visible }) {
   const [movies, setMovies] = useState([]);
+  const [currentMovieIndex, setCurrentMovieIndex] = useState(0);
+
   useEffect(() => {
     fetchMovies().then((moviesData) => {
-      setMovies(moviesData);
+      setMovies(moviesData.slice(0, 10));
     });
   }, []);
+
+  const handleNextMovie = () => {
+    setCurrentMovieIndex((prevIndex) => (prevIndex + 1) % movies.length);
+  };
+
+  const handlePreviousMovie = () => {
+    setCurrentMovieIndex(
+      (prevIndex) => (prevIndex - 1 + movies.length) % movies.length
+    );
+  };
 
   return (
     <>
       <div
-        className={`container mx-auto flex flex-col px-0 justify-center items-center pb-10 ${visible}`}
+        className={`container mx-auto flex justify-center items-center ${visible}`}
       >
-        <div className="flex gap-x-8">
-          {console.log(movies.slice(0, 10))}
-          <MoviesList movieArray={movies.slice(0, 10)} />
+        <div className="flex items-center justify-center">
+          <div
+            className="cursor-pointer text-3xl pr-6"
+            onClick={handlePreviousMovie}
+          >
+            &lt;
+          </div>
+          <div className=" px-8 py-8 mb-4 border-2  shadow-2xl border-black flex items-center space-x-12 ">
+            {console.log(movies)}
+
+            {/* <img
+              className="absolute scale-150"
+              src={`https://image.tmdb.org/t/p/w500${movies[currentMovieIndex].backdrop_path}`}
+              alt=""
+            /> */}
+            <img
+              src={`https://image.tmdb.org/t/p/w300${movies[currentMovieIndex].backdrop_path}`}
+              alt=""
+            />
+            <div class="">
+              <h2 class="text-lg sm:text-xl text-black font-semibold title-font">
+                {movies[currentMovieIndex].original_title}
+              </h2>
+              <p class="leading-relaxed text-md mb-4 text-gray-600">
+                {`Release date : ${movies[currentMovieIndex].release_date}`}
+              </p>
+              <p class="leading-relaxed text-md mb-4 text-black font-semibold">
+                {movies[currentMovieIndex].overview}
+              </p>
+            </div>
+          </div>
+
+          <div
+            className="cursor-pointer text-3xl pl-6"
+            onClick={handleNextMovie}
+          >
+            &gt;
+          </div>
         </div>
       </div>
     </>
   );
 }
+// I have a hero array in javascript that contains a movies array. I want to show the first movie object and also add arrows the the sides of the object. A left arrow on the left and a right arrow on the right. When i click on the right arrow, the current movie object fades away and the next movie object fades in. When i click on the left arrow, same thing happens but the previous movie object takes place.
 
 const fetchMovies = () => {
   const options = {
