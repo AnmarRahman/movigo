66e7b7f5cd36a201c1bb508bf577e2c60f937011
AnmaritoS <95002766+AnmaritoS@users.noreply.github.com>
2023-07-30 01:36:08 -0400

    footer screen adjustments done

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,12 +3,12 @@ function Footer() {
     <>
       <footer className="text-gray-600 bg-[#1b1b1b]">
         <div className="container px-5 py-10 mx-auto">
-          <div className="flex flex-wrap justify-center items-center md:text-left text-center order-first">
-            <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-              <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">
+          <div className="flex justify-center items-center md:text-left text-center order-first text-sm lg:text-base">
+            <div className="lg:w-1/4 md:w-1/2 w-full px-4 ">
+              <h2 className="title-font font-medium text-white tracking-widest mb-3">
                 CATEGORIES
               </h2>
-              <nav className="list-none mb-0">
+              <nav className="list-none mb-0 ">
                 <li>
                   <a className="text-white hover:text-gray-500 cursor-pointer">
                     Now Playing
@@ -89,8 +89,8 @@ function Footer() {
               {/* <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">
                 SUBSCRIBE
               </h2> */}
-              <div className="flex xl:flex-nowrap md:flex-nowrap lg:flex-wrap flex-wrap justify-center items-end md:justify-start">
-                <div className="relative w-40 sm:w-auto xl:mr-4 lg:mr-0 sm:mr-4 mr-2">
+              <div className="flex xl:flex-nowrap md:flex-wrap flex-wrap justify-center items-end md:justify-start xl:space-y-0 space-y-2 xl:space-x-4 space-x-0">
+                <div className="relative w-auto">
                   <input
                     type="text"
                     id="footer-field"
@@ -98,7 +98,7 @@ function Footer() {
                     className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                   />
                 </div>
-                <button className="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+                <button className="w-[100px] flex justify-center items-center lg:mt-2 xl:mt-0 flex-shrink-0 text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
                   Subscribe
                 </button>
               </div>
